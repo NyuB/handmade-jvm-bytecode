c460c8c9bbc1beb52c1ab0fe4357338a4e1f498f
NyuB <brice.decaestecker@gmx.fr>
2023-02-26 12:05:58 +0100

    Add a TestClassLoader for programmatic java class file validation

---
+++ java/TestClassLoader.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class TestClassLoader extends ClassLoader {
+    public Class<?> findClass(String name) {
+        byte[] b = readCustomClassFile(name);
+        return defineClass(name, b, 0, b.length);
+    }
+
+    public byte[] readCustomClassFile(String name) {
+        try {
+            return Files.readAllBytes(Paths.get(String.format("target/classes/%s.class", name)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, SecurityException {
+        Class<?> pythonClass = new TestClassLoader().findClass(args[0]);
+        System.out.println(pythonClass);
+        try {
+            Object instance = pythonClass.getConstructors()[0].newInstance();
+            System.out.println(instance);
+        } catch(VerifyError e) {
+            System.out.println("Invalid class constructor bytecode");
+            throw e;
+        } catch (InstantiationException e) {
+            System.out.println("default Constructor of the class cannot be invoked");
+            throw e;
+        } catch (InvocationTargetException e) {
+            System.out.println("Error thrown by constructor method");
+            throw e;
+        } 
+    }
+
+}
