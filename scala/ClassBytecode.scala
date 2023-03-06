8be699f1ea691e6eb2153181c6a5eb00437c6714
NyuB <brice.decaestecker@gmx.fr>
2023-03-06 04:13:44 +0100

    Applied scala format

--- scala/ClassBytecode.scala
+++ scala/ClassBytecode.scala
@@ -1,95 +1,119 @@
 object ClassBytecode:
-    type Bytes = IArray[Byte]
-    
-    opaque type ConstantPoolIndex = Int
-    object ConstantPoolIndex:
-        def fromZeroBasedIndex(zeroBasedIndex: Int): ConstantPoolIndex = zeroBasedIndex + 1
-        
-    extension(cpi: ConstantPoolIndex)
-        def toBytes: Bytes = u2(cpi)
+  type Bytes = IArray[Byte]
 
-    def byteLength(bytesArray: IArray[Bytes]): Int =
-        bytesArray.foldLeft(0)((total, bytes) => total + bytes.size)
+  opaque type ConstantPoolIndex = Int
+  object ConstantPoolIndex:
+    def fromZeroBasedIndex(zeroBasedIndex: Int): ConstantPoolIndex =
+      zeroBasedIndex + 1
 
-    val MAGIC_NUMBER: Bytes = u4(0xCAFEBABE)
-    val VERSION_JAVA_8_MAJOR: Bytes = u2(52)
-    val VERSION_JAVA_8_MINOR: Bytes = u2(0)
+  extension (cpi: ConstantPoolIndex) def toBytes: Bytes = u2(cpi)
 
-    val CLASSFILE_STRING_TAG: Bytes = u1(1)
-    val CLASSFILE_CLASS_TAG: Bytes = u1(7)
-    val CLASSFILE_METHOD_REF_TAG: Bytes = u1(10)
-    val CLASSFILE_NAME_AND_TYPE_DESCRIPTOR_TAG: Bytes = u1(12)
+  def byteLength(bytesArray: IArray[Bytes]): Int =
+    bytesArray.foldLeft(0)((total, bytes) => total + bytes.size)
 
-    val EMPTY_TABLE: Bytes = elementCount(0) // [0 elements, []]
+  val MAGIC_NUMBER: Bytes = u4(0xcafebabe)
+  val VERSION_JAVA_8_MAJOR: Bytes = u2(52)
+  val VERSION_JAVA_8_MINOR: Bytes = u2(0)
 
-    val PUBLIC_FLAG = 0x1
-    val FINAL_FLAG = 0x10
+  val CLASSFILE_STRING_TAG: Bytes = u1(1)
+  val CLASSFILE_CLASS_TAG: Bytes = u1(7)
+  val CLASSFILE_METHOD_REF_TAG: Bytes = u1(10)
+  val CLASSFILE_NAME_AND_TYPE_DESCRIPTOR_TAG: Bytes = u1(12)
 
-    def flags(flagList: List[Int]) = u2(flagList.foldLeft(0)((acc, i) => acc | i))
+  val EMPTY_TABLE: Bytes = elementCount(0) // [0 elements, []]
 
-    def stringUtf8(s: String): Bytes =
-        val stringBytes = IArray.from(s.getBytes(java.nio.charset.StandardCharsets.UTF_8))
-        IArray(CLASSFILE_STRING_TAG, u2(stringBytes.length), stringBytes).flatten
+  val PUBLIC_FLAG = 0x1
+  val FINAL_FLAG = 0x10
 
-    def classNameReference(classNameIndex: ConstantPoolIndex): Bytes =
-        IArray(CLASSFILE_CLASS_TAG, classNameIndex.toBytes).flatten
+  def flags(flagList: List[Int]) = u2(flagList.foldLeft(0)((acc, i) => acc | i))
 
-    def elementCount(count: Int): Bytes = u2(count)
+  def stringUtf8(s: String): Bytes =
+    val stringBytes =
+      IArray.from(s.getBytes(java.nio.charset.StandardCharsets.UTF_8))
+    IArray(CLASSFILE_STRING_TAG, u2(stringBytes.length), stringBytes).flatten
 
-    def constantPoolSection(constantPool: IArray[Bytes]): Bytes =
-        IArray(
-            elementCount(constantPool.size + 1), // constant pool size = len(constant pool) + 1
-            constantPool.flatten
-        ).flatten
+  def classNameReference(classNameIndex: ConstantPoolIndex): Bytes =
+    IArray(CLASSFILE_CLASS_TAG, classNameIndex.toBytes).flatten
 
-    def u1(i: Int): Bytes = IArray(i.toByte)
+  def elementCount(count: Int): Bytes = u2(count)
 
-    def u2(i: Int): Bytes =
-        val left = (i  & 0xFF00) >> 8
-        val right = i & 0xFF
-        IArray(left.toByte, right.toByte)
+  def constantPoolSection(constantPool: IArray[Bytes]): Bytes =
+    IArray(
+      elementCount(
+        constantPool.size + 1
+      ), // constant pool size = len(constant pool) + 1
+      constantPool.flatten
+    ).flatten
 
-    def u4(i: Int): Bytes =
-        val a = (i & 0xFF000000) >> 24
-        val b = (i & 0x00FF0000) >> 16
-        val c = (i & 0x0000FF00) >> 8
-        val d = (i & 0x000000FF)
-        IArray(a, b, c, d).map(_.toByte)
+  def u1(i: Int): Bytes = IArray(i.toByte)
 
-    val aload_0 = u1(0x2A)
-    val invokespecial = u1(0xB7)
-    val _return = u1(0xB1)
+  def u2(i: Int): Bytes =
+    val left = (i & 0xff00) >> 8
+    val right = i & 0xff
+    IArray(left.toByte, right.toByte)
 
-    def method(codeAttributeIndex: ConstantPoolIndex, stackSize: Int, localSize: Int)(operations: IArray[Bytes]): Bytes =
-        val body = IArray(
-            u2(stackSize),
-            u2(localSize),
-            u4(byteLength(operations)),
-            operations.flatten,
-            EMPTY_TABLE,
-            EMPTY_TABLE,
-        )
+  def u4(i: Int): Bytes =
+    val a = (i & 0xff000000) >> 24
+    val b = (i & 0x00ff0000) >> 16
+    val c = (i & 0x0000ff00) >> 8
+    val d = (i & 0x000000ff)
+    IArray(a, b, c, d).map(_.toByte)
 
-        IArray(
-            codeAttributeIndex.toBytes,
-            u4(byteLength(body)),
-            body.flatten,
-        ).flatten
-    
-    def constructor(codeAttributeIndex: ConstantPoolIndex, superInitIndex: ConstantPoolIndex): Bytes = method(codeAttributeIndex, 1, 1) {
-        IArray(
-            aload_0,
-            IArray(invokespecial, superInitIndex.toBytes).flatten,
-            _return 
-        )
-    }
+  val aload_0 = u1(0x2a)
+  val invokespecial = u1(0xb7)
+  val _return = u1(0xb1)
 
-    def methodReference(receiverClassIndex: ConstantPoolIndex, nameAndTypeDescriptorIndex: ConstantPoolIndex): Bytes =
-        IArray(CLASSFILE_METHOD_REF_TAG, receiverClassIndex.toBytes, nameAndTypeDescriptorIndex.toBytes).flatten
-    
-    def methodNameAndTypeDescriptor(nameIndex: ConstantPoolIndex, typeDescriptorIndex: ConstantPoolIndex): Bytes = 
-        IArray(CLASSFILE_NAME_AND_TYPE_DESCRIPTOR_TAG, nameIndex.toBytes, typeDescriptorIndex.toBytes).flatten
-    
-    def methodTypeDescriptor(args: List[String], returnType: String): Bytes =
-        val joinedArgs = args.foldLeft("")((s, a) => s ++ a)
-        stringUtf8(s"(${joinedArgs})${returnType}")
+  def method(
+      codeAttributeIndex: ConstantPoolIndex,
+      stackSize: Int,
+      localSize: Int
+  )(operations: IArray[Bytes]): Bytes =
+    val body = IArray(
+      u2(stackSize),
+      u2(localSize),
+      u4(byteLength(operations)),
+      operations.flatten,
+      EMPTY_TABLE,
+      EMPTY_TABLE
+    )
+
+    IArray(
+      codeAttributeIndex.toBytes,
+      u4(byteLength(body)),
+      body.flatten
+    ).flatten
+
+  def constructor(
+      codeAttributeIndex: ConstantPoolIndex,
+      superInitIndex: ConstantPoolIndex
+  ): Bytes = method(codeAttributeIndex, 1, 1) {
+    IArray(
+      aload_0,
+      IArray(invokespecial, superInitIndex.toBytes).flatten,
+      _return
+    )
+  }
+
+  def methodReference(
+      receiverClassIndex: ConstantPoolIndex,
+      nameAndTypeDescriptorIndex: ConstantPoolIndex
+  ): Bytes =
+    IArray(
+      CLASSFILE_METHOD_REF_TAG,
+      receiverClassIndex.toBytes,
+      nameAndTypeDescriptorIndex.toBytes
+    ).flatten
+
+  def methodNameAndTypeDescriptor(
+      nameIndex: ConstantPoolIndex,
+      typeDescriptorIndex: ConstantPoolIndex
+  ): Bytes =
+    IArray(
+      CLASSFILE_NAME_AND_TYPE_DESCRIPTOR_TAG,
+      nameIndex.toBytes,
+      typeDescriptorIndex.toBytes
+    ).flatten
+
+  def methodTypeDescriptor(args: List[String], returnType: String): Bytes =
+    val joinedArgs = args.foldLeft("")((s, a) => s ++ a)
+    stringUtf8(s"(${joinedArgs})${returnType}")
--- scala/JvmClass.scala
+++ scala/JvmClass.scala
@@ -2,119 +2,156 @@ import ClassBytecode._
 
 sealed trait ConstantPoolEntry
 object ConstantPoolEntry:
-    case class RawString(s: String) extends ConstantPoolEntry
-    case class ClassNameReference(s: String) extends ConstantPoolEntry
-    case class MethodTypeDescriptor(paramsTypes: List[String], returnType: String) extends ConstantPoolEntry
-    case class MethodReference(receiverClass: ClassNameReference, nameAndTypeDescriptor: MethodNameAndTypeDescriptor) extends ConstantPoolEntry
-    case class MethodNameAndTypeDescriptor(name: String, methodTypeDescriptor: MethodTypeDescriptor) extends ConstantPoolEntry
+  case class RawString(s: String) extends ConstantPoolEntry
+  case class ClassNameReference(s: String) extends ConstantPoolEntry
+  case class MethodTypeDescriptor(paramsTypes: List[String], returnType: String)
+      extends ConstantPoolEntry
+  case class MethodReference(
+      receiverClass: ClassNameReference,
+      nameAndTypeDescriptor: MethodNameAndTypeDescriptor
+  ) extends ConstantPoolEntry
+  case class MethodNameAndTypeDescriptor(
+      name: String,
+      methodTypeDescriptor: MethodTypeDescriptor
+  ) extends ConstantPoolEntry
 
-case class ConstantPool(entries: List[ConstantPoolEntry]):    
-    def indexOf(entry: ConstantPoolEntry): ConstantPoolIndex =
-        val index = entries.indexOf(entry)
-        if index < 0 then
-            throw IllegalStateException(s"Unknown entry in constant pool ${entry}")
-        else
-            ConstantPoolIndex.fromZeroBasedIndex(index)
-    
-    def toBytesArray: IArray[Bytes] =
-        IArray.from(entries.map(bytesFromEntry(_)))
+case class ConstantPool(entries: List[ConstantPoolEntry]):
+  def indexOf(entry: ConstantPoolEntry): ConstantPoolIndex =
+    val index = entries.indexOf(entry)
+    if index < 0 then
+      throw IllegalStateException(s"Unknown entry in constant pool ${entry}")
+    else ConstantPoolIndex.fromZeroBasedIndex(index)
 
-    private def bytesFromEntry(entry: ConstantPoolEntry): Bytes =
-        entry match
-            case ConstantPoolEntry.RawString(s) =>
-                ClassBytecode.stringUtf8(s)
+  def toBytesArray: IArray[Bytes] =
+    IArray.from(entries.map(bytesFromEntry(_)))
 
-            case ConstantPoolEntry.ClassNameReference(className) =>
-                val index = this.indexOf(ConstantPoolEntry.RawString(className))
-                ClassBytecode.classNameReference(index)
+  private def bytesFromEntry(entry: ConstantPoolEntry): Bytes =
+    entry match
+      case ConstantPoolEntry.RawString(s) =>
+        ClassBytecode.stringUtf8(s)
 
-            case ConstantPoolEntry.MethodTypeDescriptor(params, returnType) =>
-                ClassBytecode.methodTypeDescriptor(params, returnType)
+      case ConstantPoolEntry.ClassNameReference(className) =>
+        val index = this.indexOf(ConstantPoolEntry.RawString(className))
+        ClassBytecode.classNameReference(index)
 
-            case ConstantPoolEntry.MethodReference(receiverClass, nameAndTypeDescriptor) =>
-                val receiverIndex = this.indexOf(receiverClass)
-                val nameAndDescriptorIndex = this.indexOf(nameAndTypeDescriptor)
-                ClassBytecode.methodReference(receiverIndex, nameAndDescriptorIndex)
+      case ConstantPoolEntry.MethodTypeDescriptor(params, returnType) =>
+        ClassBytecode.methodTypeDescriptor(params, returnType)
 
-            case ConstantPoolEntry.MethodNameAndTypeDescriptor(name, typeDescriptor) =>
-                val nameIndex = this.indexOf(ConstantPoolEntry.RawString(name))
-                val typeDescriptorIndex = this.indexOf(typeDescriptor)
-                ClassBytecode.methodNameAndTypeDescriptor(nameIndex, typeDescriptorIndex)
+      case ConstantPoolEntry.MethodReference(
+            receiverClass,
+            nameAndTypeDescriptor
+          ) =>
+        val receiverIndex = this.indexOf(receiverClass)
+        val nameAndDescriptorIndex = this.indexOf(nameAndTypeDescriptor)
+        ClassBytecode.methodReference(receiverIndex, nameAndDescriptorIndex)
 
-case class JvmClass private (thisName: String, superName: String, constantPool: ConstantPool, methods: List[ClassBytecode.Bytes]):
-    class MutablePool(constantPool: ConstantPool):
-        val entries = scala.collection.mutable.ArrayBuffer.from(constantPool.entries)
-        def getOrAppend(entry: ConstantPoolEntry): ConstantPoolIndex =
-            val index = entries.indexOf(entry)
-            if index < 0 then
-                entries += entry
-                ConstantPoolIndex.fromZeroBasedIndex(entries.size - 1)
-            else
-                ConstantPoolIndex.fromZeroBasedIndex(index)
-        def toConstantPool: ConstantPool = ConstantPool(List.from(entries))
+      case ConstantPoolEntry.MethodNameAndTypeDescriptor(
+            name,
+            typeDescriptor
+          ) =>
+        val nameIndex = this.indexOf(ConstantPoolEntry.RawString(name))
+        val typeDescriptorIndex = this.indexOf(typeDescriptor)
+        ClassBytecode.methodNameAndTypeDescriptor(
+          nameIndex,
+          typeDescriptorIndex
+        )
 
-    def withMethod(methodDeclaration: (MutablePool) => (Bytes)): JvmClass =
-        val mutablePool = MutablePool(constantPool)
-        val bytes = methodDeclaration(mutablePool)
-        this.copy(constantPool = mutablePool.toConstantPool, methods = this.methods :+ bytes)
-        
-    def toBytes: Bytes =
-        val thisIndex = constantPool.indexOf(ConstantPoolEntry.ClassNameReference(thisName))
-        val superIndex = constantPool.indexOf(ConstantPoolEntry.ClassNameReference(superName))
-        val cp = constantPool.toBytesArray
-        IArray(
-            MAGIC_NUMBER,
-            VERSION_JAVA_8_MINOR,
-            VERSION_JAVA_8_MAJOR,
-            ClassBytecode.constantPoolSection(cp),
-            flags(List(PUBLIC_FLAG, FINAL_FLAG)),
-            thisIndex.toBytes, // this
-            superIndex.toBytes, // super
-            EMPTY_TABLE, // no interface
-            EMPTY_TABLE, // no field
-            u2(methods.size),
-            IArray.from(methods).flatten,
-            EMPTY_TABLE, // no attribute
-        ).flatten
+case class JvmClass private (
+    thisName: String,
+    superName: String,
+    constantPool: ConstantPool,
+    methods: List[ClassBytecode.Bytes]
+):
+  class MutablePool(constantPool: ConstantPool):
+    val entries =
+      scala.collection.mutable.ArrayBuffer.from(constantPool.entries)
+    def getOrAppend(entry: ConstantPoolEntry): ConstantPoolIndex =
+      val index = entries.indexOf(entry)
+      if index < 0 then
+        entries += entry
+        ConstantPoolIndex.fromZeroBasedIndex(entries.size - 1)
+      else ConstantPoolIndex.fromZeroBasedIndex(index)
+    def toConstantPool: ConstantPool = ConstantPool(List.from(entries))
+
+  def withMethod(methodDeclaration: (MutablePool) => (Bytes)): JvmClass =
+    val mutablePool = MutablePool(constantPool)
+    val bytes = methodDeclaration(mutablePool)
+    this.copy(
+      constantPool = mutablePool.toConstantPool,
+      methods = this.methods :+ bytes
+    )
+
+  def toBytes: Bytes =
+    val thisIndex =
+      constantPool.indexOf(ConstantPoolEntry.ClassNameReference(thisName))
+    val superIndex =
+      constantPool.indexOf(ConstantPoolEntry.ClassNameReference(superName))
+    val cp = constantPool.toBytesArray
+    IArray(
+      MAGIC_NUMBER,
+      VERSION_JAVA_8_MINOR,
+      VERSION_JAVA_8_MAJOR,
+      ClassBytecode.constantPoolSection(cp),
+      flags(List(PUBLIC_FLAG, FINAL_FLAG)),
+      thisIndex.toBytes, // this
+      superIndex.toBytes, // super
+      EMPTY_TABLE, // no interface
+      EMPTY_TABLE, // no field
+      u2(methods.size),
+      IArray.from(methods).flatten,
+      EMPTY_TABLE // no attribute
+    ).flatten
 
 object JvmClass:
-    def apply(thisName: String, superName: String): JvmClass =
-        JvmClass(thisName, superName, ConstantPool(List(
-            ConstantPoolEntry.RawString(thisName),
-            ConstantPoolEntry.ClassNameReference(thisName),
-            ConstantPoolEntry.RawString(superName),
-            ConstantPoolEntry.ClassNameReference(superName),
-        )), List.empty)
+  def apply(thisName: String, superName: String): JvmClass =
+    JvmClass(
+      thisName,
+      superName,
+      ConstantPool(
+        List(
+          ConstantPoolEntry.RawString(thisName),
+          ConstantPoolEntry.ClassNameReference(thisName),
+          ConstantPoolEntry.RawString(superName),
+          ConstantPoolEntry.ClassNameReference(superName)
+        )
+      ),
+      List.empty
+    )
 
-val customJvmClass = 
-    JvmClass("Crafted", "java/lang/Object")
-    .withMethod { cp => 
-        val name = ConstantPoolEntry.RawString("<init>")
-        val codeAttribute = ConstantPoolEntry.RawString("Code")
-        val descriptor = ConstantPoolEntry.MethodTypeDescriptor(List.empty, "V")
-        val nameAndTypeDescriptor = ConstantPoolEntry.MethodNameAndTypeDescriptor("<init>", descriptor)
-        val objectClassReference = ConstantPoolEntry.ClassNameReference("java/lang/Object")
-        val objectInit = ConstantPoolEntry.MethodReference(objectClassReference, nameAndTypeDescriptor)
+val customJvmClass =
+  JvmClass("Crafted", "java/lang/Object")
+    .withMethod { cp =>
+      val name = ConstantPoolEntry.RawString("<init>")
+      val codeAttribute = ConstantPoolEntry.RawString("Code")
+      val descriptor = ConstantPoolEntry.MethodTypeDescriptor(List.empty, "V")
+      val nameAndTypeDescriptor =
+        ConstantPoolEntry.MethodNameAndTypeDescriptor("<init>", descriptor)
+      val objectClassReference =
+        ConstantPoolEntry.ClassNameReference("java/lang/Object")
+      val objectInit = ConstantPoolEntry.MethodReference(
+        objectClassReference,
+        nameAndTypeDescriptor
+      )
 
-        val nameIndex = cp.getOrAppend(name)
-        val codeIndex = cp.getOrAppend(codeAttribute)
-        val descriptorIndex = cp.getOrAppend(descriptor)
-        val nameAndDescriptorIndex = cp.getOrAppend(nameAndTypeDescriptor)
-        val superInitIndex = cp.getOrAppend(objectInit)
+      val nameIndex = cp.getOrAppend(name)
+      val codeIndex = cp.getOrAppend(codeAttribute)
+      val descriptorIndex = cp.getOrAppend(descriptor)
+      val nameAndDescriptorIndex = cp.getOrAppend(nameAndTypeDescriptor)
+      val superInitIndex = cp.getOrAppend(objectInit)
 
-        IArray(
-            flags(List(PUBLIC_FLAG)),
-            nameIndex.toBytes,
-            descriptorIndex.toBytes,
-            elementCount(1),
-            constructor(codeIndex, superInitIndex),
-        ).flatten
+      IArray(
+        flags(List(PUBLIC_FLAG)),
+        nameIndex.toBytes,
+        descriptorIndex.toBytes,
+        elementCount(1),
+        constructor(codeIndex, superInitIndex)
+      ).flatten
     }
 
 @main def writeBytes(path: String) =
-    val file = java.io.File(path)
-    val target = java.io.BufferedOutputStream(java.io.FileOutputStream(file))
-    try
-        customJvmClass.toBytes.foreach( target.write(_) )
-    finally 
-        target.close
+  val file = java.io.File(path)
+  val target = java.io.BufferedOutputStream(java.io.FileOutputStream(file))
+  try
+    customJvmClass.toBytes.foreach(target.write(_))
+  finally
+    target.close
